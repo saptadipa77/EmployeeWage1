6cd993c39898a2f8cdb0222d275cde9fa8c79338
saptadipa77 <saptadipadatta5@gmail.com>
2025-12-10 14:35:06 +0530

    add third uc

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         System.out.println("Welcome to Employee Wage Computation Program");
         AttendanceCheck ac = new AttendanceCheck();
         DailyWage dw = new DailyWage();
+        Parttime pt = new Parttime();
         System.out.println("UC1 Attendance: " + ac.getAttendance());
         System.out.println("UC2 Daily Wage: " + dw.calculateDailyWage());
+        System.out.println("UC3 Part-Time Wage: " + pt.calculatePartTimeWage());
     }
 }
---
+++ src/Parttime.java
@@ -0,0 +1,10 @@
+public class Parttime {
+
+    final int wagePerHour = 20;
+    final int partTimeHours = 4;
+
+    public int calculatePartTimeWage() {
+        return wagePerHour * partTimeHours;
+    }
+}
+
