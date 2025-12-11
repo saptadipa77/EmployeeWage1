92f98472a90d894b33af57a2cb97cffec8922dc4
saptadipa77 <saptadipadatta5@gmail.com>
2025-12-12 00:58:12 +0530

    add 11th uc

---
+++ src/Iwagecomputation.java
@@ -0,0 +1,6 @@
+public interface Iwagecomputation {
+    public void addCompany(String name, int rate, int days, int hours);
+    public void computeWages();
+    public int getTotalWage(String companyName);
+}
+
